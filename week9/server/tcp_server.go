747ca0541f74e112b8ca7db29af71c906ce9a940
Cxb <569262726@qq.com>
2021-07-31 17:54:15 +0800

    write2client

--- week9/server/tcp_server.go
+++ week9/server/tcp_server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	protocol2 "go-up/week9/protocol"
 	"net"
+	"time"
 )
 
 // 协议解析失败，如何处理（报错并关闭连接）
@@ -16,6 +17,7 @@ func main() {
 	}
 	for {
 		conn, err := listener.Accept()
+		//conn.SetReadDeadline()
 		fmt.Println("------Accept-----", conn.RemoteAddr())
 		if err != nil {
 			panic(err)
@@ -28,9 +30,27 @@ func main() {
 				fmt.Printf("Receive Msg: %s\n", info.Body)
 				// TODO 根据收到消息，调用接口，返回数据
 				rtn := fmt.Sprintf("Ack for %s", info.Body)
-				conn.Write(proto.Encode([]byte(rtn)))
+				_, err = conn.Write(proto.Encode([]byte(rtn)))
+				if err != nil {
+				    panic(err)
+				}
 			}
 		}()
 		//}()
+	}
+}
+
+func tryWrite2Client(conn net.Conn, proto protocol2.Protocol) {
+	index := 0
+	for {
+		index += 1
+		msg := fmt.Sprintf("Server msg %d", index)
+		_, err := conn.Write(proto.Encode([]byte(msg)))
+		if err != nil {
+			fmt.Println("Error:", time.Now())
+			panic(err)
+		}
+		fmt.Println(time.Now(), msg)
+		time.Sleep(time.Second * 5)
 	}
 }
